0dcd81be331080f390ae9056f6940b593a8aad94
Hugo Wride <hugowride@gmail.com>
2023-06-11 10:03:39 +0100

    Basic Vite project.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+    </>
+  )
+}
+
+export default App

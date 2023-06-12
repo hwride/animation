322c6876c28f95294cf2e280a92a592390c8c88b
Hugo Wride <hugowride@gmail.com>
2023-06-12 10:11:35 +0100

    More styling.

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,21 @@ function App() {
 
   return (
     <div className="flex h-full text-left">
-      <ol className="border-black border-r list-none m-0 p-4">
-        <li>
-          <ListButton onClick={() => setSelectedExample(undefined)}>
-            Empty
-          </ListButton>
-        </li>
-        <li>
-          <ListButton onClick={() => setSelectedExample('simpleCoordAnimate')}>
-            Simple coordinate animation
-          </ListButton>
-        </li>
-      </ol>
+      <div className="flex flex-col border-black border-r p-4">
+        <ListButton onClick={() => setSelectedExample(undefined)}>
+          Empty
+        </ListButton>
+        <h2 className="text-lg font-bold my-2">Framer Motion</h2>
+        <ol className="flex-1 list-none m-0">
+          <li>
+            <ListButton
+              onClick={() => setSelectedExample('simpleCoordAnimate')}
+            >
+              Simple coordinate animation
+            </ListButton>
+          </li>
+        </ol>
+      </div>
       <div className="flex-1">
         {selectedExample === 'simpleCoordAnimate' && <SimpleCoordAnimate />}
         {selectedExample == null && (

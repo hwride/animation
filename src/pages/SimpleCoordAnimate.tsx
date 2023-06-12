751af77cfdcb68afdd857bb811d843944473d3f7
Hugo Wride <hugowride@gmail.com>
2023-06-12 10:16:23 +0100

    Fix jumping text.

--- src/pages/SimpleCoordAnimate.tsx
+++ src/pages/SimpleCoordAnimate.tsx
@@ -3,35 +3,42 @@ import { useState } from 'react'
 export function SimpleCoordAnimate() {
   const [coordVal, setCoordVal] = useState(50)
   return (
-    <div className="w-min m-auto pt-4">
-      <label>
-        Animate
-        <input
-          value={coordVal}
-          type="range"
-          min={0}
-          max={100}
-          onChange={(e) => setCoordVal(parseFloat(e.target.value))}
-        />
-        {/* Absolute to stop it affecting width when the numbers increase */}
-        <span className="absolute">{coordVal}px</span>
-      </label>
-      <motion.div
-        animate={{ x: -coordVal }}
-        className="w-min border border-gray-100 my-4"
-      >
-        <pre className="m-0">
-          &lt;motion.div animate={'{{'} x: -{coordVal} {'}}'}&gt;
-        </pre>
-      </motion.div>
-      <motion.div
-        animate={{ x: coordVal }}
-        className="w-min border border-gray-100"
-      >
-        <pre className="m-0">
-          &lt;motion.div animate={'{{'} x: {coordVal} {'}}'}&gt;
-        </pre>
-      </motion.div>
-    </div>
+    <>
+      <div className="w-fit m-auto pt-4">
+        {/* Controls */}
+        <label>
+          <input
+            className="mx-2"
+            value={coordVal}
+            type="range"
+            min={0}
+            max={100}
+            onChange={(e) => setCoordVal(parseFloat(e.target.value))}
+          />
+          {/* Absolute to stop it affecting width when the numbers increase */}
+          <span className="absolute">{coordVal}px</span>
+        </label>
+      </div>
+
+      <div className="w-min m-auto pt-4">
+        {/* Animated stuff */}
+        <motion.div
+          animate={{ x: -coordVal }}
+          className="w-min border border-gray-100 my-4"
+        >
+          <pre className="m-0">
+            &lt;motion.div animate={'{{'} x: -{coordVal} {'}}'}&gt;
+          </pre>
+        </motion.div>
+        <motion.div
+          animate={{ x: coordVal }}
+          className="w-min border border-gray-100"
+        >
+          <pre className="m-0">
+            &lt;motion.div animate={'{{'} x: {coordVal} {'}}'}&gt;
+          </pre>
+        </motion.div>
+      </div>
+    </>
   )
 }

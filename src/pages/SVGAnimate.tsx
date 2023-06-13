ef641be7c67ff9c9eeaae8c57cdc0e6025f1d37a
Hugo Wride <hugowride@gmail.com>
2023-06-13 20:27:29 +0100

    Add some extra props to the SVG animation example.

--- src/pages/SVGAnimate.tsx
+++ src/pages/SVGAnimate.tsx
@@ -5,13 +5,20 @@ import { TextInput } from '../components/TextInput.tsx'
 import { H2, H3 } from '../components/Headings.tsx'
 import { Page } from '../components/Page.tsx'
 
-function getVariants(opts: { pathLength: number; duration: number }) {
+function getVariants(opts: {
+  pathLength: number
+  pathSpacing: number
+  pathOffset: number
+  duration: number
+}) {
   return {
     hidden: {
       pathLength: 0,
     },
     visible: {
       pathLength: opts.pathLength,
+      pathSpacing: opts.pathSpacing,
+      pathOffset: opts.pathOffset,
       transition: { duration: opts.duration },
     },
   }
@@ -20,9 +27,13 @@ function getVariants(opts: { pathLength: number; duration: number }) {
 export function SVGAnimate() {
   const [key, setKey] = useState(0)
   const [pathLength, setPathLength] = useState('1')
+  const [pathSpacing, setPathSpacing] = useState('1')
+  const [pathOffset, setPathOffset] = useState('0')
   const [duration, setDuration] = useState('1')
   const drawVariants = getVariants({
     pathLength: Number(pathLength),
+    pathSpacing: Number(pathSpacing),
+    pathOffset: Number(pathOffset),
     duration: Number(duration),
   })
   const reRenderSvg = () => setKey((key) => key + 1)
@@ -32,7 +43,7 @@ export function SVGAnimate() {
       <div>
         <H3>Options</H3>
         <div className="grid grid-cols-2 mx-auto w-max">
-          <LabelledTextInput
+          <PathRangeInput
             id="path-length"
             label={<code>pathLength</code>}
             value={pathLength}
@@ -41,12 +52,32 @@ export function SVGAnimate() {
               reRenderSvg()
             }}
           />
-          <LabelledTextInput
-            id="duration"
-            label={<code>duration</code>}
-            value={duration}
+          <PathRangeInput
+            id="path-spacing"
+            label={<code>pathSpacing</code>}
+            value={pathSpacing}
             onChange={(val) => {
-              setDuration(val)
+              setPathSpacing(val)
+              reRenderSvg()
+            }}
+          />
+          <PathRangeInput
+            id="path-offset"
+            label={<code>pathOffset</code>}
+            value={pathOffset}
+            onChange={(val) => {
+              setPathOffset(val)
+              reRenderSvg()
+            }}
+          />
+          <label htmlFor="duration">{<code>duration</code>}</label>
+          <TextInput
+            id="duration"
+            value={duration}
+            type="number"
+            step=".5"
+            onChange={(e) => {
+              setDuration(e.target.value)
               reRenderSvg()
             }}
           />
@@ -103,7 +134,10 @@ export function SVGAnimate() {
   )
 }
 
-function LabelledTextInput({
+/**
+ * Internal util component defining a range between 0 and 1 with a step of .1.
+ */
+function PathRangeInput({
   id,
   label,
   value,
@@ -117,9 +151,13 @@ function LabelledTextInput({
   return (
     <>
       <label htmlFor={id}>{label}</label>
-      <TextInput
+      <input
         id={id}
         value={value}
+        type="range"
+        min="0"
+        max="1"
+        step=".1"
         onChange={(e) => onChange(e.target.value)}
       />
     </>

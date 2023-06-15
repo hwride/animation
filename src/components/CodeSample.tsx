80b51656c6e332acef2975eb4684bdd7368936a6
Hugo Wride <hugowride@gmail.com>
2023-06-15 19:41:04 +0100

    Reduce bundle size by using light build of react-syntax-highlighter.

--- src/components/CodeSample.tsx
+++ src/components/CodeSample.tsx
@@ -1,13 +1,16 @@
 import {
-  Prism as SyntaxHighlighter,
+  PrismLight as SyntaxHighlighter,
   SyntaxHighlighterProps,
 } from 'react-syntax-highlighter'
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx'
+import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism'
+
+SyntaxHighlighter.registerLanguage('jsx', jsx)
 
 export function CodeSample({ children, ...overrides }: SyntaxHighlighterProps) {
   // wrapLongLines helps with mobile view.
   return (
-    <SyntaxHighlighter style={dark} wrapLongLines={true} {...overrides}>
+    <SyntaxHighlighter style={prism} wrapLongLines={true} {...overrides}>
       {children}
     </SyntaxHighlighter>
   )

71ff0f5519896c0b5de60cee7e3943d05acacc24
Hugo Wride <hugowride@gmail.com>
2023-06-18 15:24:59 +0100

    Menu button styling tweaks.

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -117,8 +117,7 @@ function MenuContent({
             <motion.li
               key={i}
               className="relative"
-              initial={{}}
-              whileHover={{ scale: 1.05 }}
+              whileHover={{ scale: 1.04, transition: { duration: 0.05 } }}
             >
               <ListButton
                 className="w-full"
